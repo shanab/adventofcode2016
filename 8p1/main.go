b49f57a48d89d128ba5e511f1ed1b5b7fc5348e2
Ahmed <ahmed@sybogames.com>
2016-12-13 12:00:17 +0100

    Day 8 solution

---
+++ 8p1/main.go
@@ -0,0 +1,308 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const PROBLEM_STATEMENT = `
+--- Day 8: Two-Factor Authentication ---
+
+You come across a door implementing what you can only assume is an implementation of two-factor authentication after a long game of requirements telephone.
+
+To get past the door, you first swipe a keycard (no problem; there was one on a nearby desk). Then, it displays a code on a little screen, and you type that code on a keypad. Then, presumably, the door unlocks.
+
+Unfortunately, the screen has been smashed. After a few minutes, you've taken everything apart and figured out how it works. Now you just have to work out what the screen would have displayed.
+
+The magnetic strip on the card you swiped encodes a series of instructions for the screen; these instructions are your puzzle input. The screen is 50 pixels wide and 6 pixels tall, all of which start off, and is capable of three somewhat peculiar operations:
+
+rect AxB turns on all of the pixels in a rectangle at the top-left of the screen which is A wide and B tall.
+rotate row y=A by B shifts all of the pixels in row A (0 is the top row) right by B pixels. Pixels that would fall off the right end appear at the left end of the row.
+rotate column x=A by B shifts all of the pixels in column A (0 is the left column) down by B pixels. Pixels that would fall off the bottom appear at the top of the column.
+For example, here is a simple sequence on a smaller screen:
+
+rect 3x2 creates a small rectangle in the top-left corner:
+
+###....
+###....
+.......
+rotate column x=1 by 1 rotates the second column down by one pixel:
+
+#.#....
+###....
+.#.....
+rotate row y=0 by 4 rotates the top row right by four pixels:
+
+....#.#
+###....
+.#.....
+rotate column x=1 by 1 again rotates the second column down by one pixel, causing the bottom pixel to wrap back to the top:
+
+.#..#.#
+#.#....
+.#.....
+As you can see, this display technology is extremely powerful, and will soon dominate the tiny-code-displaying-screen market. That's what the advertisement on the back of the display tries to convince you, anyway.
+
+There seems to be an intermediate check of the voltage used by the display: after you swipe your card, if the screen did work, how many pixels should be lit?`
+
+const INPUT = `rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 2
+rect 1x2
+rotate row y=1 by 5
+rotate row y=0 by 3
+rect 1x2
+rotate column x=30 by 1
+rotate column x=25 by 1
+rotate column x=10 by 1
+rotate row y=1 by 5
+rotate row y=0 by 2
+rect 1x2
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 4x1
+rotate row y=2 by 18
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 3x1
+rotate row y=2 by 12
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 4x1
+rotate column x=20 by 1
+rotate row y=2 by 5
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 4x1
+rotate row y=2 by 15
+rotate row y=0 by 15
+rotate column x=10 by 1
+rotate column x=5 by 1
+rotate column x=0 by 1
+rect 14x1
+rotate column x=37 by 1
+rotate column x=23 by 1
+rotate column x=7 by 2
+rotate row y=3 by 20
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 4x1
+rotate row y=3 by 5
+rotate row y=2 by 2
+rotate row y=1 by 4
+rotate row y=0 by 4
+rect 1x4
+rotate column x=35 by 3
+rotate column x=18 by 3
+rotate column x=13 by 3
+rotate row y=3 by 5
+rotate row y=2 by 3
+rotate row y=1 by 1
+rotate row y=0 by 1
+rect 1x5
+rotate row y=4 by 20
+rotate row y=3 by 10
+rotate row y=2 by 13
+rotate row y=0 by 10
+rotate column x=5 by 1
+rotate column x=3 by 3
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate row y=4 by 10
+rotate row y=3 by 10
+rotate row y=1 by 10
+rotate row y=0 by 10
+rotate column x=7 by 2
+rotate column x=5 by 1
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate row y=4 by 20
+rotate row y=3 by 12
+rotate row y=1 by 15
+rotate row y=0 by 10
+rotate column x=8 by 2
+rotate column x=7 by 1
+rotate column x=6 by 2
+rotate column x=5 by 1
+rotate column x=3 by 1
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate column x=46 by 2
+rotate column x=43 by 2
+rotate column x=24 by 2
+rotate column x=14 by 3
+rotate row y=5 by 15
+rotate row y=4 by 10
+rotate row y=3 by 3
+rotate row y=2 by 37
+rotate row y=1 by 10
+rotate row y=0 by 5
+rotate column x=0 by 3
+rect 3x3
+rotate row y=5 by 15
+rotate row y=3 by 10
+rotate row y=2 by 10
+rotate row y=0 by 10
+rotate column x=7 by 3
+rotate column x=6 by 3
+rotate column x=5 by 1
+rotate column x=3 by 1
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate column x=19 by 1
+rotate column x=10 by 3
+rotate column x=5 by 4
+rotate row y=5 by 5
+rotate row y=4 by 5
+rotate row y=3 by 40
+rotate row y=2 by 35
+rotate row y=1 by 15
+rotate row y=0 by 30
+rotate column x=48 by 4
+rotate column x=47 by 3
+rotate column x=46 by 3
+rotate column x=45 by 1
+rotate column x=43 by 1
+rotate column x=42 by 5
+rotate column x=41 by 5
+rotate column x=40 by 1
+rotate column x=33 by 2
+rotate column x=32 by 3
+rotate column x=31 by 2
+rotate column x=28 by 1
+rotate column x=27 by 5
+rotate column x=26 by 5
+rotate column x=25 by 1
+rotate column x=23 by 5
+rotate column x=22 by 5
+rotate column x=21 by 5
+rotate column x=18 by 5
+rotate column x=17 by 5
+rotate column x=16 by 5
+rotate column x=13 by 5
+rotate column x=12 by 5
+rotate column x=11 by 5
+rotate column x=3 by 1
+rotate column x=2 by 5
+rotate column x=1 by 5
+rotate column x=0 by 1`
+
+const GRID_WIDTH = 50
+const GRID_HEIGHT = 6
+
+type Grid struct {
+	grid [][]bool
+}
+
+func NewGrid(width, height int) *Grid {
+	g := &Grid{}
+	g.grid = make([][]bool, height)
+	for i := 0; i < height; i++ {
+		g.grid[i] = make([]bool, width)
+	}
+
+	return g
+}
+
+func (g *Grid) Illuminate(width, height int) {
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			g.grid[i][j] = true
+		}
+	}
+}
+
+func (g *Grid) RotateRow(index, rotations int) {
+	g.grid[index] = rotateRight(g.grid[index], rotations)
+}
+
+func rotateRight(array []bool, rotations int) []bool {
+	for i := 0; i < rotations; i++ {
+		array = append([]bool{array[len(array)-1]}, array[:len(array)-1]...)
+	}
+
+	return array
+}
+
+func (g *Grid) RotateColumn(index, rotations int) {
+	column := make([]bool, len(g.grid))
+	for i := 0; i < len(column); i++ {
+		column[i] = g.grid[i][index]
+	}
+
+	column = rotateRight(column, rotations)
+	for i := 0; i < len(column); i++ {
+		g.grid[i][index] = column[i]
+	}
+}
+
+func main() {
+	fmt.Println(PixelsLit())
+}
+
+func PixelsLit() int {
+	var count int
+	operations := strings.Split(INPUT, "\n")
+	g := NewGrid(GRID_WIDTH, GRID_HEIGHT)
+
+	for _, operation := range operations {
+		switch operation[:3] {
+		case "rec":
+			boundsArray := strings.Split(operation[5:], "x")
+			width, _ := strconv.Atoi(boundsArray[0])
+			height, _ := strconv.Atoi(boundsArray[1])
+			g.Illuminate(width, height)
+		case "rot":
+			fields := strings.Fields(operation)
+			index, _ := strconv.Atoi(fields[2][2:])
+			rotations, _ := strconv.Atoi(fields[4])
+
+			switch operation[:8] {
+			case "rotate r":
+				g.RotateRow(index, rotations)
+			case "rotate c":
+				g.RotateColumn(index, rotations)
+			}
+		}
+	}
+
+	for i := 0; i < len(g.grid); i++ {
+		for j := 0; j < len(g.grid[0]); j++ {
+			if g.grid[i][j] {
+				count++
+			}
+		}
+	}
+
+	return count
+}
